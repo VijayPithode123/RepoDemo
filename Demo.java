df1d1bc91b07c04e38a5542532b12a252bdfa00d
Vijay <vijaypithode1997@gmail.com>
2022-12-22 13:59:16 +0530

    Collection code

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.git;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		LinkedList l=new LinkedList();
+		l.add("Vijay");
+		l.add("Pihtode");
+		
+		System.out.println(l);
+
+	}
+
+}
---
+++ SimpleGit.java
@@ -0,0 +1,11 @@
+package com.git;
+import java.util.*;
+public class SimpleGit {
+
+	public static void main(String[] args) {
+		 
+		ArrayList a=new ArrayList();
+
+	}
+
+}
